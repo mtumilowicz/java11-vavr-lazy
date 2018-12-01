f9694c8b8dabcead30143aa66bd63576e1ac8332
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-01 11:45:34 +0100

    LazyInstanceMethodsTest.java

--- src/test/java/LazyInstanceMethodsTest.java
+++ src/test/java/LazyInstanceMethodsTest.java
@@ -1,10 +1,31 @@
+import io.vavr.Lazy;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by mtumilowicz on 2018-12-01.
  */
 public class LazyInstanceMethodsTest {
     @Test
-    public void isEmpty_null() {
+    public void isEvaluated_true() {
+        Lazy<String> lazy = Lazy.of(() -> "evaluated");
+
+        assertFalse(lazy.isEvaluated());
+
+        lazy.get();
+
+        assertTrue(lazy.isEvaluated());
+    }
+
+    @Test
+    public void map_returns_notEvaluated() {
+        Lazy<String> lazy = Lazy.of(() -> "evaluated");
+        lazy.get();
+        assertTrue(lazy.isEvaluated());
+
+        Lazy<String> mapped = lazy.map(str -> str + "");
+        assertFalse(mapped.isEvaluated());
     }
 }
